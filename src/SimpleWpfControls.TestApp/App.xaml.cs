13884303af2af209131169c1a8c4884d483ed1bb
bobahml <bobahml@mail.ru>
2014-10-26 18:13:22 +0300

    TestApp: added display exceptions.

--- src/SimpleWpfControls.TestApp/App.xaml.cs
+++ src/SimpleWpfControls.TestApp/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
 using Autofac;
 using SimpleWpfControls.TestApp.ViewModels;
 using SimpleWpfControls.TestApp.Views;
@@ -10,12 +12,27 @@ namespace SimpleWpfControls.TestApp
     /// </summary>
     public partial class App
     {
-        protected override void OnStartup(StartupEventArgs e)
+        protected override void OnStartup(StartupEventArgs ea)
         {
-            base.OnStartup(e);
+            base.OnStartup(ea);
 
-            MainWindow = new MainView {DataContext = Container.AutofacContainer.Resolve<MainViewModel>()};
+            AppDomain.CurrentDomain.UnhandledException += (s, e) => LogUnhandledException((Exception)e.ExceptionObject, "AppDomain.CurrentDomain.UnhandledException");
+            DispatcherUnhandledException += (s, e) => LogUnhandledException(e.Exception, "Application.Current.DispatcherUnhandledException");
+            TaskScheduler.UnobservedTaskException += (s, e) => LogUnhandledException(e.Exception, "TaskScheduler.UnobservedTaskException");
+
+
+
+            MainWindow = new MainView { DataContext = Container.AutofacContainer.Resolve<MainViewModel>() };
             MainWindow.Show();
+        }
+
+
+
+        private static void LogUnhandledException(Exception exceptionObject, string source)
+        {
+            MessageBox.Show(
+                string.Format("Необработанное исключение ({0}): {1}{2}", source, Environment.NewLine,
+                exceptionObject));
         }
     }
 }
--- src/SimpleWpfControls.TestApp/Container.cs
+++ src/SimpleWpfControls.TestApp/Container.cs
@@ -12,8 +12,6 @@ namespace SimpleWpfControls.TestApp
             _container = RegisterDependencies();
         }
 
-
-
         public static IContainer AutofacContainer
         {
             get { return _container; }
